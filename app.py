2f5c0173deedad47f05c51b49762f0c04f656c77
harikartik <harikartik2015@gmail.com>
2024-02-08 11:59:06 +0530

    commit changes

--- app.py
+++ app.py
@@ -13,29 +13,10 @@ class UrlRepo(db.Model):
     target = db.Column(db.Integer, default = 0)
     timeStamp = db.Column(db.DateTime, default = datetime.utcnow)
 
-    # def __repr__(self):
-    #     return '<URL: %r>' % self.url
-
 #bind function to an url
-@app.route('/home', methods=['POST','GET']) 
+@app.route('/home') 
 def index():
-
-    # if request.method == 'POST':
-    #     input_url = request.form['url']
-    #     new_url = UrlRepo(url=input_url)
-
-    #     try:
-    #         db.session.add(new_url)
-    #         db.session.commit()
-    #         url = UrlRepo.query.order_by(UrlRepo.timeStamp).all()
-    #         return render_template('repo.html', url=url)
-    #         #return redirect('repo.html', url=url)
-        
-    #     except:
-    #         return "Cannot add URL to the repo"
-
-    # else:
-        return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/phishing_repo', methods=["POST"])
 def show_repo():
@@ -51,14 +32,11 @@ def show_repo():
             #return redirect('repo.html', url=url)
         
         except:
+            db.session.rollback()
             return "Cannot add URL to the repo"
         
     if request.method == "GET":
          return redirect("repo.html")
-
-# @app.route('/dynamicURL/<username>')
-# def show_user(username):
-#     return f"Hello {username}!"
 
 if __name__ == "__main__":
     app.run(debug=True)
